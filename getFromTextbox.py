b4777d66c7f024fddecfe21851e92b2758907b87
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-07 10:12:47 +0900

    フレームの作成を修正

--- getFromTextbox.py
+++ getFromTextbox.py
@@ -1,7 +1,6 @@
 # テキストボックスに入力されている文字列を取得して、メッセージを表示する
 
 import tkinter as tk
-import tkinter.ttk as ttk
 from tkinter import messagebox
 
 # 関数namingの定義
@@ -15,18 +14,24 @@ root.title("文字列の取得・クリア")
 root.geometry("600x800")
 
 # フレームの作成と配置
-frame = ttk.Frame(root) #rootウィンドウにフレームを作成
-frame.grid(column=0, row=0, sticky=tk.NSEW, padx=5, pady=10)
+frame = tk.Frame(
+    root, width=600, height=400, bg="alice blue",padx=60, pady=30
+) 
+#frame.grid(column=0, row=0, sticky=tk.NSEW, padx=5, pady=10)
+frame.pack()
 
 # 各種ウィジェットの作成
-label = ttk.Label(frame, text = "入力してください。")
-entry = ttk.Entry(frame)
-button_execute = ttk.Button(frame, text="作成", command=naming)
+label = tk.Label(frame, text = "入力してください。", bg="alice blue")
+entry = tk.Entry(frame)
+button_execute = tk.Button(frame, text="作成", command=naming)
 
 # 各種ウィジェットの設置
-label.grid(row=0, column=0)
-entry.grid(row=0, column=1)
-button_execute.grid(row=1, column=1)
+#label.grid(row=0, column=0)
+#entry.grid(row=0, column=1)
+#button_execute.grid(row=1, column=1)
+label.pack()
+entry.pack()
+button_execute.pack()
 
 # Entryウィジェットへ文字列をセットしておく
 entry.insert(tk.END,"新しいプロジェクト")
